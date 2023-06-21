de4c41ce591d8b5e8cca20f9e9baee5a625a9384
s181482 <64785412+s181482@users.noreply.github.com>
2023-06-21 10:57:55 +0200

    Update HammingKode.m

--- HammingKode.m
+++ HammingKode.m
@@ -4,30 +4,51 @@
 
 %Hamming enkoder 
 %N er antal bit K er infromations bit 
-% Hamming (N,K)
+% Hamming (N,K) %encode realtion mellem N og K
+
 N=7;
 K=4;
+m=N-K;
 
-% H Paritets matrix
-H=zeros(N,(N-K));   % N eller (2^(N-K))-1
+%%informations bits 
+x=zeros(K);
+for i =1:K 
+    x(i,:)= randi([0 1],1,K); %informations vektor 
+end   
+
+
+% H Paritetscheck matrix
+H=zeros(N,m);     % N eller (2^(N-K))-1 % m eller (N-K)
 j=N;
 L=1;
 for i=1:N           %((2^(N-K))-1 = N måske skal der kun står N
-
     if (i^i)==i || all(factor(i)==2)
-        H(j,:)=flip(de2bi(i,(N-K)));
+        H(j,:)=flip(de2bi(i,m));
         j=j-1;
     else
-        H(L,:)=flip(de2bi(i,(N-K)));
+        H(L,:)=flip(de2bi(i,m));
         L=L+1;
     end
 end
 
 % G generator matrix
-G=[eye(K) H(1:K,:)];
+G=[eye(K) H(1:K,:)]; %[egenmatrix paritetscheck]
 
-%informations bits 
-x= randi([0 1],1,K);
-
-%encoder 
-c=mod(x*G,2);
+%enkoder 
+% c = xG
+%c=mod(x*G,2); %kodeord
+%% Produktkode
+P=zeros(N);
+%række
+for i=1:K 
+    c=mod(x(i,:)*G,2);
+    P(i,:)=mod(x(i,:)*G,2);
+end
+%søjle
+P=P';
+for i=1:N
+    P(i,:)=mod(P(i,1:K)*G,2);
+end
+P=P';
+%tjek x og P(4x4)
+%x == P(1:K,1:K)
